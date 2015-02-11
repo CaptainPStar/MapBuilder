52fa6cc19d131ca793ae49c83c5db046d8003c3e
NeoArmageddon <neoarmageddon@web.de>
2015-02-11 22:58:46 +0100

    Versionnumber increased

--- @MapBuilder/mod.cpp
+++ @MapBuilder/mod.cpp
@@ -1,4 +1,4 @@
 name = "Map Builder";
-actionName = "Map Builder 0.6 Alpha";
-action = "http://www.modfact.net";
-version = "0.6";
+actionName = "Map Builder 0.7 Alpha";
+action = "http://mb.modfact.com";
+version = "0.7";
