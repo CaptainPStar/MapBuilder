2634e6c7a474d13c75e67a7967a83c406afcd863
NeoArmageddon <neoarmageddon@web.de>
2015-03-29 19:04:46 +0200

    Changed padding

--- MapBuilder.VR/MapBuilder/ui/about.hpp
+++ MapBuilder.VR/MapBuilder/ui/about.hpp
@@ -56,7 +56,7 @@
 			//type = CT_STATIC;
 			style = ST_PICTURE + ST_KEEP_ASPECT_RATIO;
 			idc = -1;
-			x =  2*MB_WINDOW_PADDING_X + MB_WINDOW_GRID_X * 0;
+			x =  1*MB_WINDOW_PADDING_X + MB_WINDOW_GRID_X * 0;
 			y = MB_WINDOW_PADDING_Y + MB_WINDOW_GRID_Y * 1;
 			w = MB_WINDOW_GRID_X * 2;
 			h = MB_WINDOW_GRID_Y * 2;
@@ -67,7 +67,7 @@
 		class MB_Window_About_CurVersionLabel : RscText {
 			idc = -1;
 			text = "Current Version:";
-			x =  2*MB_WINDOW_PADDING_X + MB_WINDOW_GRID_X * 2;
+			x =  1*MB_WINDOW_PADDING_X + MB_WINDOW_GRID_X * 2;
 			y = MB_WINDOW_PADDING_Y + MB_WINDOW_GRID_Y * 1;
 			w = MB_WINDOW_GRID_X * 6;
 			h = MB_WINDOW_GRID_Y * 1;
@@ -75,7 +75,7 @@
 		class MB_Window_About_CurVersion : RscText {
 			idc = -1;
 			text = MB_VERSION;
-			x =  2*MB_WINDOW_PADDING_X + MB_WINDOW_GRID_X * 8;
+			x =  1*MB_WINDOW_PADDING_X + MB_WINDOW_GRID_X * 8;
 			y = MB_WINDOW_PADDING_Y + MB_WINDOW_GRID_Y * 1;
 			w = MB_WINDOW_GRID_X * 6;
 			h = MB_WINDOW_GRID_Y * 1;
@@ -83,7 +83,7 @@
 		class MB_Window_About_NewVersionLabel : RscText {
 			idc = -1;
 			text = "Newest Version:";
-			x =  2*MB_WINDOW_PADDING_X + MB_WINDOW_GRID_X * 2;
+			x =  1*MB_WINDOW_PADDING_X + MB_WINDOW_GRID_X * 2;
 			y = MB_WINDOW_PADDING_Y + MB_WINDOW_GRID_Y * 2;
 			w = MB_WINDOW_GRID_X * 6;
 			h = MB_WINDOW_GRID_Y * 1;
@@ -91,7 +91,7 @@
 		class MB_Window_About_NewVersion : RscText {
 			idc = 170601;
 			text = "Unknown";
-			x =  2*MB_WINDOW_PADDING_X + MB_WINDOW_GRID_X * 8;
+			x =  1*MB_WINDOW_PADDING_X + MB_WINDOW_GRID_X * 8;
 			y = MB_WINDOW_PADDING_Y + MB_WINDOW_GRID_Y * 2;
 			w = MB_WINDOW_GRID_X * 6;
 			h = MB_WINDOW_GRID_Y * 1;
@@ -99,7 +99,7 @@
 		class MB_Window_About_VersionWarning : RscText {
 			idc = 170602;
 			text = "";
-			x =  2*MB_WINDOW_PADDING_X + MB_WINDOW_GRID_X * 0;
+			x =  1*MB_WINDOW_PADDING_X + MB_WINDOW_GRID_X * 0;
 			y = MB_WINDOW_PADDING_Y + MB_WINDOW_GRID_Y * 3;
 			w = MB_WINDOW_GRID_X * 14;
 			h = MB_WINDOW_GRID_Y * 1;
@@ -107,7 +107,7 @@
 		class MB_Window_About_nCredits : RscStructuredText {
 			idc = 170603;
 			text = "";
-			x =  2*MB_WINDOW_PADDING_X + MB_WINDOW_GRID_X * 0;
+			x =  1*MB_WINDOW_PADDING_X + MB_WINDOW_GRID_X * 0;
 			y = MB_WINDOW_PADDING_Y + MB_WINDOW_GRID_Y * 5;
 			w = MB_WINDOW_GRID_X * 18;
 			h = MB_WINDOW_GRID_Y * 13;
