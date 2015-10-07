2234fca2870c4b8779bc2051f78888d8dfe91f80
NeoArmageddon <neoarmageddon@web.de>
2015-10-07 08:29:22 +0200

    Added badbensons p3d objects

---
+++ @MapBuilder/AddOns/MapBuilder/ui/gizmo.hpp
@@ -0,0 +1,30 @@
+class ArrowY
+	{
+
+		onObjectMoved = "systemChat str _this";
+
+		idc = -1; 
+		type = 82;
+		model = "\MB\MapBuilder\helper\pfeil";
+		//model = "\A3\Structures_F\Items\Food\Can_V3_F.p3d";
+		scale = 1;
+
+		direction[] = {0.5, 0, 0.5};
+		up[] = {0.5, 0, 1}; 
+
+		//position[] = {0,0,0.2}; optional
+
+		x = 0.5;
+		y = 0.5;
+		z = 0.2;
+
+		//positionBack[] = {0,0,1.2}; optional
+
+		xBack = 0.5;
+		yBack = 0.5;
+		zBack = 1.2;
+
+		inBack = 1;
+		enableZoom = 1;
+		zoomDuration = 0.001;
+	};
